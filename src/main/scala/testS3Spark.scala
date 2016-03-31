cd32d9a76d1ea887d028bd2205cf2f321bbdb9bf
Eva Garcia <egarcia@datiobd.com>
2016-03-31 10:42:09 +0200

    Examples with Amazon S3 files and Spark/CrossData added.

---
+++ src/main/scala/testS3Spark.scala
@@ -0,0 +1,39 @@
+/**
+  * Created by egarcia on 28/03/16.
+  */
+
+import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.sql.SQLContext
+
+object testS3Spark  {
+  def main(args: Array[String]): Unit = {
+    //Spark Context
+    val conf = new SparkConf().setMaster("local[2]").setAppName("TestS3")
+    val sc = new SparkContext(conf)
+
+    //S3 Authentication
+    val AccessKey = "<AccessKey>"
+    val SecretAccessKey = "<SecretAccessKey>"
+
+    //S3 Data
+    val bucket = "<bucket>"
+    val file = "<file>"
+
+    //Temporary Table Data for Creation
+    val nameTable = "<nameTable>"
+    val atributesTable = "<atributesTable>"
+
+    //Hadoop Configuration
+    sc.hadoopConfiguration.set("fs.s3n.impl", "org.apache.hadoop.fs.s3native.NativeS3FileSystem")
+    sc.hadoopConfiguration.set("fs.s3n.awsAccessKeyId", AccessKey )
+    sc.hadoopConfiguration.set("fs.s3n.awsSecretAccessKey", SecretAccessKey)
+
+    //SQL Context
+    val scXData = new SQLContext(sc)
+
+    // S3 Treatment
+    scXData.sql("CREATE TEMPORARY TABLE "+nameTable+atributesTable+" USING com.databricks.spark.csv OPTIONS (path 's3n://"+AccessKey+":"+SecretAccessKey+"@"+bucket+"/"+file+"')")
+    scXData.sql("SELECT * FROM segmentacion").show()
+
+  }
+}
